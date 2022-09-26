3730824260cd8f493866faa6b8c69062ef868f22
Ryan Virtue <ryan.virtue@mrjaffesclass.com>
2022-09-26 12:24:47 -0700

    i hope this adds

---
+++ johnlennon/JohnLennon.java
@@ -0,0 +1,35 @@
+package johnlennon;
+
+
+/**
+ * Write a description of class JohnLennon here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class JohnLennon
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class JohnLennon
+     */
+    public JohnLennon()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
